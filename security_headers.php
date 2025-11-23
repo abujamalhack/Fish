a4f404aa7c845006903a336220ead9544ed6de38
𝐴𝑏𝑢 𝐽𝑎𝑚𝑎𝑙 | CyberPhantom <mccarthy29mp@gmail.com>
2025-11-23 17:44:45 +0300

    Create security_headers.php

---
+++ security_headers.php
@@ -0,0 +1,9 @@
+<?php
+// أمان إضافي
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains');
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: DENY');
+header('X-XSS-Protection: 1; mode=block');
+header('Referrer-Policy: no-referrer');
+header('Permissions-Policy: geolocation=(), microphone=(), camera=()');
+?>
